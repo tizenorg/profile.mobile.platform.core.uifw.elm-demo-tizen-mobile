4f7f1ca87a6056a3ff303a4400c00e5596b0bc98
ChunEon Park <chuneon.park@samsung.com>
2015-04-30 17:45:12 +0900

    button: refactoring.
    re-layout the buttons in horizontal box for nicer view.
    add descrive comments additionally.
    
    Change-Id: Icf5559eb3e58633b5b61a3d4891bfee1df9ce07f

--- src/button.c
+++ src/button.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -40,54 +40,68 @@ create_button_view(Evas_Object *parent)
 {
 	Evas_Object *btn, *img, *box;
 
+	/* box */
 	box = elm_box_add(parent);
 	evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	evas_object_size_hint_align_set(box, EVAS_HINT_FILL, EVAS_HINT_FILL);
-	elm_box_padding_set(box, 0, 5 * elm_config_scale_get());
 	evas_object_show(box);
 
 	/* icon_reorder style */
 	btn = elm_button_add(box);
 	elm_object_style_set(btn, "icon_reorder");
 	evas_object_smart_callback_add(btn, "clicked", btn_clicked_cb, (void *)1);
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	evas_object_show(btn);
 	elm_box_pack_end(box, btn);
+
 	/* icon_expand_add style */
 	btn = elm_button_add(box);
 	elm_object_style_set(btn, "icon_expand_add");
 	evas_object_smart_callback_add(btn, "clicked", btn_clicked_cb, (void *)2);
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	evas_object_show(btn);
 	elm_box_pack_end(box, btn);
+
 	/* icon_expand_delete style */
 	btn = elm_button_add(box);
 	elm_object_style_set(btn, "icon_expand_delete");
 	evas_object_smart_callback_add(btn, "clicked", btn_clicked_cb, (void *)3);
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	evas_object_show(btn);
 	elm_box_pack_end(box, btn);
+
 	/* default style */
 	btn = elm_button_add(box);
 	elm_object_text_set(btn, "default");
 	evas_object_size_hint_min_set(btn, ELM_SCALE_SIZE(150), ELM_SCALE_SIZE(58));
 	evas_object_smart_callback_add(btn, "clicked", btn_clicked_cb, (void *)4);
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	evas_object_show(btn);
 	elm_box_pack_end(box, btn);
+
 	/* btn_circle style */
 	btn = elm_button_add(box);
 	elm_object_style_set(btn, "circle");
 	elm_object_text_set(btn, "circle twoline");
+
+	 /* btn_circle icon */
 	img = elm_image_add(btn);
 	elm_image_file_set(img, ICON_DIR"/core_icon_brightness.png", NULL);
 	elm_image_resizable_set(img, EINA_TRUE, EINA_TRUE);
 	elm_object_part_content_set(btn, "icon", img);
+
 	evas_object_smart_callback_add(btn, "clicked", btn_clicked_cb, (void *)5);
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	evas_object_show(btn);
 	elm_box_pack_end(box, btn);
+
 	/* bottom style */
 	btn = elm_button_add(box);
 	elm_object_style_set(btn, "bottom");
 	elm_object_text_set(btn, "bottom");
 	evas_object_size_hint_min_set(btn, ELM_SCALE_SIZE(150), ELM_SCALE_SIZE(58));
 	evas_object_smart_callback_add(btn, "clicked", btn_clicked_cb, (void *)6);
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	evas_object_show(btn);
 	elm_box_pack_end(box, btn);
 
@@ -96,12 +110,15 @@ create_button_view(Evas_Object *parent)
 	elm_object_style_set(btn, "contacts");
 	elm_object_text_set(btn, "contacts");
 	evas_object_smart_callback_add(btn, "clicked", btn_clicked_cb, (void *)7);
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	evas_object_show(btn);
 	elm_box_pack_end(box, btn);
 
+	/* option style */
 	btn = elm_button_add(box);
 	elm_object_style_set(btn, "option");
 	evas_object_smart_callback_add(btn, "clicked", btn_clicked_cb, (void *)8);
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	evas_object_show(btn);
 	elm_box_pack_end(box, btn);
 
