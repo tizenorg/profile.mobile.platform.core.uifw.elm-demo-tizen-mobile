38b8432ea6bc11ab0aca48b86c168e5b98a9855e
ChunEon Park <chuneon.park@samsung.com>
2015-05-12 14:37:08 +0900

    conformant: revised example.
    Change-Id: I923b1a194f74b14ea0a5871bd3257beebed8ef99

--- src/conformant.c
+++ src/conformant.c
@@ -17,66 +17,42 @@
 
 #include "main.h"
 
-static Evas_Object *ctxpopup = NULL;
-
 static void
-ctxpopup_dismissed_cb(void *data, Evas_Object *obj, void *event_info)
-{
-	evas_object_del(ctxpopup);
-	ctxpopup = NULL;
-}
-
-static void
-move_ctxpopup(Evas_Object *ctxpopup, Evas_Object *btn)
-{
-	Evas_Coord x, y, w , h;
-	evas_object_geometry_get(btn, &x, &y, &w, &h);
-	evas_object_move(ctxpopup, (w / 2), y + (h / 2));
-}
-
-static void
-ctxpopup_item_select_cb(void *data, Evas_Object *obj, void *event_info)
+opaque_btn_clicked_cb(void *data, Evas_Object *obj, void *event_info)
 {
 	appdata_s *ad = data;
-	const char *label = elm_object_item_text_get((Elm_Object_Item *) event_info);
-
+	elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_OPAQUE);
 	elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_SHOW);
 
-	if (!strcmp(label, "INDICATOR_OPAQUE"))
-		elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_OPAQUE);
-	else if (!strcmp(label, "INDICATOR_TRANSLUCENT"))
-		elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_TRANSLUCENT);
-	else if (!strcmp(label, "INDICATOR_TRANSPARENT"))
-		elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_TRANSPARENT);
-	else
-		elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_BG_TRANSPARENT);
-
-	evas_object_del(ctxpopup);
-	ctxpopup = NULL;
+	/* Unset if you set the Indicator BG */
+	evas_object_del(elm_object_part_content_get(ad->conform, "elm.swallow.indicator_bg"));
 }
 
 static void
-indicator_btn_cb(void *data, Evas_Object *obj, void *event_info)
+translucent_btn_clicked_cb(void *data, Evas_Object *obj, void *event_info)
 {
 	appdata_s *ad = data;
-	Evas_Object *nf = ad->nf;
+	elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_SHOW);
+	elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_TRANSLUCENT);
+}
 
-	if (!strcmp(elm_object_text_get(obj), "Indicator Show"))
-	  {
-		ctxpopup = elm_ctxpopup_add(nf);
-		eext_object_event_callback_add(ctxpopup, EEXT_CALLBACK_BACK, eext_ctxpopup_back_cb, NULL);
-		evas_object_smart_callback_add(ctxpopup,"dismissed", ctxpopup_dismissed_cb, NULL);
+static void
+custom_bg_btn_clicked_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	appdata_s *ad = data;
+	/* Set Indicator BG */
+	Evas_Object *bg = evas_object_rectangle_add(evas_object_evas_get(obj));
+	evas_object_color_set(bg, 0, 0, 0, 255);
+	elm_object_part_content_set(ad->conform, "elm.swallow.indicator_bg", bg);
+	elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_OPAQUE);
+	elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_SHOW);
+}
 
-		elm_ctxpopup_item_append(ctxpopup, "INDICATOR_OPAQUE", NULL, ctxpopup_item_select_cb, ad);
-		elm_ctxpopup_item_append(ctxpopup, "INDICATOR_TRANSLUCENT", NULL, ctxpopup_item_select_cb, ad);
-		elm_ctxpopup_item_append(ctxpopup, "INDICATOR_TRANSPARENT", NULL, ctxpopup_item_select_cb, ad);
-		elm_ctxpopup_item_append(ctxpopup, "INDICATOR_BG_TRANSPARENT", NULL, ctxpopup_item_select_cb, ad);
-
-		move_ctxpopup(ctxpopup, obj);
-		evas_object_show(ctxpopup);
-	  }
-	else
-		elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_HIDE);
+static void
+hide_btn_clicked_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	appdata_s *ad = data;
+	elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_HIDE);
 }
 
 static Evas_Object*
@@ -84,23 +60,46 @@ create_conform_view(Evas_Object *parent, appdata_s *ad)
 {
 	Evas_Object *box, *btn;
 
+	/* Box */
 	box = elm_box_add(parent);
 	evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	evas_object_size_hint_align_set(box, EVAS_HINT_FILL, EVAS_HINT_FILL);
-	elm_box_padding_set(box, 0, ELM_SCALE_SIZE(50));
+	elm_box_padding_set(box, 0, ELM_SCALE_SIZE(10));
 	evas_object_show(box);
 
+	/* Indicator Opaque Button */
 	btn = elm_button_add(box);
-	elm_object_text_set(btn, "Indicator Show");
-	evas_object_size_hint_min_set(btn, ELM_SCALE_SIZE(250), ELM_SCALE_SIZE(58));
-	evas_object_smart_callback_add(btn, "clicked", indicator_btn_cb, ad);
+	elm_object_text_set(btn, "Indicator Opaque");
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	evas_object_size_hint_align_set(btn, 0.5, 0.5);
+	evas_object_smart_callback_add(btn, "clicked", opaque_btn_clicked_cb, ad);
 	evas_object_show(btn);
 	elm_box_pack_end(box, btn);
 
+	/* Indicator Translucent Button */
+	btn = elm_button_add(box);
+	elm_object_text_set(btn, "Indicator Translucent");
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	evas_object_size_hint_align_set(btn, 0.5, 0.5);
+	evas_object_smart_callback_add(btn, "clicked", translucent_btn_clicked_cb, ad);
+	evas_object_show(btn);
+	elm_box_pack_end(box, btn);
+
+	/* Indicator Custom BG Button */
+	btn = elm_button_add(box);
+	elm_object_text_set(btn, "Indicator Custom BG");
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	evas_object_size_hint_align_set(btn, 0.5, 0.5);
+	evas_object_smart_callback_add(btn, "clicked", custom_bg_btn_clicked_cb, ad);
+	evas_object_show(btn);
+	elm_box_pack_end(box, btn);
+
+	/* Indicator Hide */
 	btn = elm_button_add(box);
 	elm_object_text_set(btn, "Indicator Hide");
-	evas_object_size_hint_min_set(btn, ELM_SCALE_SIZE(250), ELM_SCALE_SIZE(58));
-	evas_object_smart_callback_add(btn, "clicked", indicator_btn_cb, ad);
+	evas_object_size_hint_weight_set(btn, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	evas_object_size_hint_align_set(btn, 0.5, 0.5);
+	evas_object_smart_callback_add(btn, "clicked", hide_btn_clicked_cb, ad);
 	evas_object_show(btn);
 	elm_box_pack_end(box, btn);
 
@@ -111,7 +110,6 @@ static Evas_Object*
 create_scroller(Evas_Object *parent)
 {
 	Evas_Object *scroller = elm_scroller_add(parent);
-	elm_object_style_set(scroller, "effect");
 	elm_scroller_policy_set(scroller, ELM_SCROLLER_POLICY_OFF, ELM_SCROLLER_POLICY_AUTO);
 	evas_object_show(scroller);
 
