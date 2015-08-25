bdb4149f32d2da7000e08d950fd78c54e714f26a
Jaeun Choi <jaeun12.choi@samsung.com>
2015-08-25 23:17:43 +0900

    spinner: remove demo cases other than vertical style
    due to elementary code diff bitween 2.4 and 3.0,
    some styles are not supported temporarily
    
    Change-Id: Iac3ff6986e8cba101218db05049ac8f06871875f

--- src/spinner.c
+++ src/spinner.c
@@ -38,47 +38,9 @@ create_spinner_view(Evas_Object *parent)
 	evas_object_show(bx);
 
 	sp = elm_spinner_add(bx);
-	evas_object_size_hint_align_set(sp, EVAS_HINT_FILL, -1.0);
-	evas_object_size_hint_weight_set(sp, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	elm_box_pack_end(bx, sp);
-	evas_object_show(sp);
-	evas_object_smart_callback_add(sp, "changed", changed_cb, NULL);
-	evas_object_smart_callback_add(sp, "delay,changed", delay_changed_cb, NULL);
-
-	sp = elm_spinner_add(bx);
-	elm_spinner_editable_set(sp, EINA_FALSE);
-	elm_spinner_label_format_set(sp, "%1.1f units");
-	elm_spinner_step_set(sp, 1.5);
-	elm_spinner_wrap_set(sp, EINA_TRUE);
-	evas_object_size_hint_align_set(sp, EVAS_HINT_FILL, -1.0);
-	evas_object_size_hint_weight_set(sp, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	elm_box_pack_end(bx, sp);
-	evas_object_show(sp);
-
-	sp = elm_spinner_add(bx);
 	elm_object_style_set(sp, "vertical");
 	elm_spinner_interval_set(sp, 0.2);
-	evas_object_size_hint_align_set(sp, EVAS_HINT_FILL, -1.0);
-	evas_object_size_hint_weight_set(sp, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	elm_box_pack_end(bx, sp);
-	evas_object_show(sp);
-
-	sp = elm_spinner_add(bx);
-	elm_spinner_editable_set(sp, EINA_FALSE);
-	elm_spinner_min_max_set(sp, 1, 12);
-	elm_spinner_special_value_add(sp, 1, "January");
-	elm_spinner_special_value_add(sp, 2, "February");
-	elm_spinner_special_value_add(sp, 3, "March");
-	elm_spinner_special_value_add(sp, 4, "April");
-	elm_spinner_special_value_add(sp, 5, "May");
-	elm_spinner_special_value_add(sp, 6, "June");
-	elm_spinner_special_value_add(sp, 7, "July");
-	elm_spinner_special_value_add(sp, 8, "August");
-	elm_spinner_special_value_add(sp, 9, "September");
-	elm_spinner_special_value_add(sp, 10, "October");
-	elm_spinner_special_value_add(sp, 11, "November");
-	elm_spinner_special_value_add(sp, 12, "December");
-	evas_object_size_hint_align_set(sp, EVAS_HINT_FILL, -1.0);
+	elm_spinner_editable_set(sp, EINA_TRUE);
 	evas_object_size_hint_weight_set(sp, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	elm_box_pack_end(bx, sp);
 	evas_object_show(sp);
