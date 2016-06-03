6e4f96c39dd6ec0bedfa44274e518305ccbd2eff
Shinwoo Kim <cinoo.kim@samsung.com>
2016-06-03 21:57:16 +0900

    [screen-reader] add example using access object
    Change-Id: I3133becad6e2c8ad0c337a976fb5c239d5c31a72

--- src/accessibility/screen_reader/description.c
+++ src/accessibility/screen_reader/description.c
@@ -45,8 +45,6 @@ create_description(Evas_Object *parent)
 	elm_object_text_set(btn, "Test Name");
 	elm_object_style_set(btn, "default");
 	elm_object_part_content_set(layout, "button_a11y_name", btn);
-	//elm_atspi_accessible_name_set(btn, "IDS_BUTTON_NAME");
-	//elm_atspi_accessible_translation_domain_set(btn, PACKAGE);
 
 	label = elm_label_add(layout);
 	elm_object_text_set(label, _("<font_size=33><align=center>Button with description</align></font_size>"));
@@ -56,8 +54,6 @@ create_description(Evas_Object *parent)
 	elm_object_text_set(btn, "Test Name");
 	elm_object_style_set(btn, "elm/button/base/default");
 	elm_object_part_content_set(layout, "button_description", btn);
-	//elm_atspi_accessible_description_set(btn, "IDS_BUTTON_DESCRIPTION");
-	//elm_atspi_accessible_translation_domain_set(btn, PACKAGE);
 
 	label = elm_label_add(layout);
 	elm_object_text_set(label, _("<font_size=33><align=center>Button without name and description</align></font_size>"));
@@ -83,4 +79,68 @@ description_cb(void *data, Evas_Object *obj, void *event_info)
 
 	layout = create_description(nf);
 	elm_naviframe_item_push(nf, "UI Description", NULL, NULL, layout, NULL);
+}
+
+static Evas_Object*
+create_description_2(Evas_Object *parent)
+{
+	Evas_Object *btn;
+	Evas_Object *layout;
+	Evas_Object *img;
+	Evas_Object *label;
+
+	layout = elm_layout_add(parent);
+	elm_layout_file_set(layout, ELM_DEMO_EDJ, "description_layout");
+	evas_object_size_hint_weight_set(layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	label = elm_label_add(layout);
+	elm_object_text_set(label, _("<font_size=33><align=center>Button with name</align></font_size>"));
+	elm_object_part_content_set(layout, "button_name_lbl", label);
+
+	btn = elm_button_add(layout);
+	elm_object_text_set(btn, "Test Name");
+	elm_object_style_set(btn, "default");
+	elm_object_part_content_set(layout, "button_name", btn);
+
+	label = elm_label_add(layout);
+	elm_object_text_set(label, _("<font_size=33><align=center>Button with accessible name</align></font_size>"));
+	elm_object_part_content_set(layout, "button_a11y_name_lbl", label);
+
+	btn = elm_button_add(layout);
+	elm_object_text_set(btn, "Test Name");
+	elm_object_style_set(btn, "default");
+	elm_object_part_content_set(layout, "button_a11y_name", btn);
+
+	label = elm_label_add(layout);
+	elm_object_text_set(label, _("<font_size=33><align=center>Button with description</align></font_size>"));
+	elm_object_part_content_set(layout, "button_description_lbl", label);
+
+	btn = elm_button_add(layout);
+	elm_object_text_set(btn, "Test Name");
+	elm_object_style_set(btn, "elm/button/base/default");
+	elm_object_part_content_set(layout, "button_description", btn);
+
+	label = elm_label_add(layout);
+	elm_object_text_set(label, _("<font_size=33><align=center>Button without name and description</align></font_size>"));
+	elm_object_part_content_set(layout, "button_empty_lbl", label);
+
+	img = elm_image_add(layout);
+	elm_image_file_set(img, ICON_DIR"/contacts_ic_circle_btn_call.png", NULL);
+	evas_object_size_hint_aspect_set(img, EVAS_ASPECT_CONTROL_VERTICAL, 1, 1);
+
+	btn = elm_button_add(layout);
+	elm_object_part_content_set(layout, "button_empty", btn);
+	elm_object_style_set(btn, "elm/button/base/circle");
+	elm_object_part_content_set(btn, "icon", img);
+
+	return layout;
+}
+
+void
+description_2_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	Evas_Object *layout;
+	Evas_Object *nf = data;
+
+	layout = create_description_2(nf);
+	elm_naviframe_item_push(nf, "UI Descriptions 2", NULL, NULL, layout, NULL);
 }
